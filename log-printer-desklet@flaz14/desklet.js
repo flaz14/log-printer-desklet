3358559ca843eae7b247178c6603bbd8e8d8cc99
flaz14 <flazfourteen@gmail.com>
2015-10-11 18:43:23 +0300

    Get rid of semicolons in tests

--- log-printer-desklet@flaz14/desklet.js
+++ log-printer-desklet@flaz14/desklet.js
@@ -545,24 +545,23 @@ LogPrinterDesklet.prototype = {
 
 ////////////////////////// TESTS //////////////////////////
 function allTests(testDir) {
-	// Print debug information:
-	global.log("RUNNING TESTS...");
-	global.log("test directory: " + testDir)
+	logInfo("RUNNING TESTS...")
+	logInfo("test directory: " + testDir)
 	
-	// Test cases (definitions):
+	// Define test cases (one function = one group of test cases):
 	let test_readLinesFromDataStream = {  
 		test_read_empty_file: function(testDir) {
-			let expected = [];
-			let dataStream = openDataStream(testDir + "empty-file.txt");
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);
+			let expected = [ ]
+			let dataStream = openDataStream(testDir + "empty-file.txt")
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 			
 		test_read_one_line_file: function(testDir) {
-			let expected = ["This is the line."];
-			let dataStream = openDataStream(testDir + "one-line-file.txt");
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);
+			let expected = ["This is the line."]
+			let dataStream = openDataStream(testDir + "one-line-file.txt")
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 
 		test_skip_one_line_and_read_the_rest: function(testDir) {
@@ -571,13 +570,13 @@ function allTests(testDir) {
 				"The third333",
 				"The fourth line.",
 				"And the fifth line is here"
-			];
-			let dataStream = openDataStream(testDir + "five-line-file.txt");
+			]
+			let dataStream = openDataStream(testDir + "five-line-file.txt")
 			// skip one line before use our function
-			dataStream.read_line(null);
+			dataStream.read_line(null)
 			// read the rest of file
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 
 		test_skip_two_line_and_read_the_rest: function(testDir) {
@@ -585,236 +584,236 @@ function allTests(testDir) {
 				"The third333",
 				"The fourth line.",
 				"And the fifth line is here"
-			];	
-			let dataStream = openDataStream(testDir + "five-line-file.txt");
+			]
+			let dataStream = openDataStream(testDir + "five-line-file.txt")
 			// skip two lines before use our function
-			dataStream.read_line(null);
-			dataStream.read_line(null);
+			dataStream.read_line(null)
+			dataStream.read_line(null)
 			// read the rest and compare
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 
 		test_skip_all_lines_and_read_the_rest: function(testDir) {
-			let expected = [ ];
-			let dataStream = openDataStream(testDir + "two-line-file.txt");
+			let expected = [ ]
+			let dataStream = openDataStream(testDir + "two-line-file.txt")
 			// skip two lines before use our function
-			dataStream.read_line(null);
-			dataStream.read_line(null);
+			dataStream.read_line(null)
+			dataStream.read_line(null)
 			// the rest of file should be empty
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 
 		test_ignore_whitespace_lines: function(testDir) {
-			let expected = ["first line", "third line"];
-			let dataStream = openDataStream(testDir + "file-with-whitespace-line.txt");
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);		
+			let expected = ["first line", "third line"]
+			let dataStream = openDataStream(testDir + "file-with-whitespace-line.txt")
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		},
 		
 		test_ignore_surrounding_whitespaces: function(testDir) {
-			let expected = ["usual line", "line with surrounding whitespaces"];
-			let dataStream = openDataStream(testDir + "file-with-surrounding-whitespaces.txt");
-			let actual = readLinesFromDataStream(dataStream);
-			assertEquals(actual, expected);		
+			let expected = ["usual line", "line with surrounding whitespaces"]
+			let dataStream = openDataStream(testDir + "file-with-surrounding-whitespaces.txt")
+			let actual = readLinesFromDataStream(dataStream)
+			assertEquals(actual, expected)
 		}		
-	};
+	}
 
 	let test_splitString = {
 		test_split_empty_string_into_zero_sized_chunks: function() {
-			let expected = [ ];
-			let actual = splitString("", 0);
-			assertEquals(actual, expected);
+			let expected = [ ]
+			let actual = splitString("", 0)
+			assertEquals(actual, expected)
 		},
 
 		test_split_non_empty_string_into_zero_sized_chunks: function() {
-			let expected = [ ];
+			let expected = [ ]
 			let actual = splitString("sample string", 0)
 			assertEquals(actual, expected);
 		},
 
 		test_split_into_chunks_2: function() {
-			let expected = ["ap", "pl", "e"];
-			let actual = splitString("apple", 2);
-			assertEquals(actual, expected);
+			let expected = ["ap", "pl", "e"]
+			let actual = splitString("apple", 2)
+			assertEquals(actual, expected)
 		},					
 		
 		test_split_into_chunks_1: function() {
-			let expected = ["a", "p", "p", "l", "e"];
-			let actual = splitString("apple", 1);
-			assertEquals(actual, expected);
+			let expected = ["a", "p", "p", "l", "e"]
+			let actual = splitString("apple", 1)
+			assertEquals(actual, expected)
 		}, 
 
 		test_split_into_chunks_3: function() {
-			let expected = ["app", "le"];
-			let actual = splitString("apple", 3);
-			assertEquals(actual, expected);
+			let expected = ["app", "le"]
+			let actual = splitString("apple", 3)
+			assertEquals(actual, expected)
 		},
 
 		test_split_into_chunks_equal_to_string_length: function() {
-			let expected = ["apple"];
-			let actual = splitString("apple", 5);
-			assertEquals(actual, expected);
+			let expected = ["apple"]
+			let actual = splitString("apple", 5)
+			assertEquals(actual, expected)
 		},
 
 		test_split_into_chunks_greater_than_string_length: function() {
-			let expected = ["apple"];
-			let actual = splitString("apple", 10);
-			assertEquals(actual, expected);
+			let expected = ["apple"]
+			let actual = splitString("apple", 10)
+			assertEquals(actual, expected)
 		},
 
 		test_split_into_chunks_with_negative_size: function() {
-			let expected = [];
-			let actual = splitString("apple", -1);
-			assertEquals(actual, expected);
+			let expected = []
+			let actual = splitString("apple", -1)
+			assertEquals(actual, expected)
 		}
-	};
+	}
 
 	let test_Screen = {
 		test_get_text_from_clear_screen: function() {
-			let expected = "";
-			let screen = new Screen(50, 6);
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = ""
+			let screen = new Screen(50, 6)
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		}, 
 
 		test_add_one_line: function() {
-			let expected = "one\n";
-			let screen = new Screen(50, 6);
-			screen.addLines( ["one"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = "one\n"
+			let screen = new Screen(50, 6)
+			screen.addLines( ["one"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 		
 		test_add_2_lines_from_6: function() {
-			let expected = "one\ntwo\n";
-			let screen = new Screen(50, 6);
-			screen.addLines( ["one", "two"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = "one\ntwo\n"
+			let screen = new Screen(50, 6)
+			screen.addLines( ["one", "two"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 
 		test_add_5_lines_from_5: function() {
-			let expected = "1\n2\n3\n4\n5\n";
-			let screen = new Screen(50, 5);
-			screen.addLines( ["1", "2", "3", "4", "5"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);		
+			let expected = "1\n2\n3\n4\n5\n"
+			let screen = new Screen(50, 5)
+			screen.addLines( ["1", "2", "3", "4", "5"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)	
 		},
 
 		test_overload_empty_screen_with_1_line: function() {
-			let expected = "2\n3\n4\n5\n6\n";
-			let screen = new Screen(50, 5);
-			screen.addLines( ["1", "2", "3", "4", "5", "6"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);		
+			let expected = "2\n3\n4\n5\n6\n"
+			let screen = new Screen(50, 5)
+			screen.addLines( ["1", "2", "3", "4", "5", "6"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)	
 		},
 		
 		test_oveload_empty_screen_with_2_lines: function() {
-			let expected = "3\n4\n5\n6\n7\n";
-			let screen = new Screen(50, 5);
-			screen.addLines( ["1", "2", "3", "4", "5", "6", "7"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);							
+			let expected = "3\n4\n5\n6\n7\n"
+			let screen = new Screen(50, 5)
+			screen.addLines( ["1", "2", "3", "4", "5", "6", "7"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 
 		test_oveload_non_empty_screen_with_3_lines: function() {
-			let expected = "4\n5\n6\n7\n8\n";
-			let screen = new Screen(50, 5);
-			screen.addLines( ["1", "2", "3", "4", "5"] );
-			screen.addLines( ["6", "7", "8"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);							
+			let expected = "4\n5\n6\n7\n8\n"
+			let screen = new Screen(50, 5)
+			screen.addLines( ["1", "2", "3", "4", "5"] )
+			screen.addLines( ["6", "7", "8"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 		
 		test_overload_screen_with_many_lines: function() {
-			let expected = "4\n5\n6\n";
-			let screen = new Screen(50, 3);
-			screen.addLines( ["1", "2", "3"] );
-			screen.addLines( ["4", "5", "6"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);									
+			let expected = "4\n5\n6\n"
+			let screen = new Screen(50, 3)
+			screen.addLines( ["1", "2", "3"] )
+			screen.addLines( ["4", "5", "6"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		}, 
 
 		test_add_lines_which_should_be_splitted: function() {
-			let expected = "appl\ne\noran\nge\nlime\n";
-			let screen = new Screen(4, 5);
-			screen.addLines( ["apple", "orange", "lime"] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);	
+			let expected = "appl\ne\noran\nge\nlime\n"
+			let screen = new Screen(4, 5)
+			screen.addLines( ["apple", "orange", "lime"] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 
 		test_add_lines_which_should_be_splitted_just_another_case: function() {
-			let expected = "s ov\ner t\nhe l\nazy \ndog.\n";
-			let screen = new Screen(4, 5);
-			screen.addLines( ["The quick brown fox jumps over the lazy dog."] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);	
+			let expected = "s ov\ner t\nhe l\nazy \ndog.\n"
+			let screen = new Screen(4, 5)
+			screen.addLines( ["The quick brown fox jumps over the lazy dog."] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 		
 		test_clear_epmty_screen: function() {
-			let expected = "";
-			let screen = new Screen(4, 5);
-			screen.clear();
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = ""
+			let screen = new Screen(4, 5)
+			screen.clear()
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 		
 		test_clear_non_epmty_screen: function() {
-			let expected = "";
-			let screen = new Screen(4, 5);
-			screen.addLines( ["This is a test", "Apple"] );
-			screen.clear();
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = ""
+			let screen = new Screen(4, 5)
+			screen.addLines( ["This is a test", "Apple"] )
+			screen.clear()
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 		
 		test_wrap_lines_is_disabled: function() {
-			let expected = "This is a test.\nI am the quick brown fox.\n";
-			let screen = new Screen(4, 50);
-			screen.disableWrapping();
-			screen.addLines( ["This is a test.", "I am the quick brown fox."] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);
+			let expected = "This is a test.\nI am the quick brown fox.\n"
+			let screen = new Screen(4, 50)
+			screen.disableWrapping()
+			screen.addLines( ["This is a test.", "I am the quick brown fox."] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
 		},
 
 		test_wrap_lines_is_enabled_explicitly: function() {
-			let expected = "This\n is \na te\nst.\nI am\n the\n qui\nck b\nrown\n fox\n.\n";
-			let screen = new Screen(4, 50);
-			screen.enableWrapping();
-			screen.addLines( ["This is a test.", "I am the quick brown fox."] );
-			let actual = screen.getText();
-			assertEquals(actual, expected);
-		}	
-	};
+			let expected = "This\n is \na te\nst.\nI am\n the\n qui\nck b\nrown\n fox\n.\n"
+			let screen = new Screen(4, 50)
+			screen.enableWrapping()
+			screen.addLines( ["This is a test.", "I am the quick brown fox."] )
+			let actual = screen.getText()
+			assertEquals(actual, expected)
+		}
+	}
 
 	let test_RegexFilter = {
 		test_pattern_is_empty: function() {
-			let filter = new RegexFilter( [""] );
-			let accepted = filter.test("apple");
-			assertEquals( accepted, true );
+			let filter = new RegexFilter( [""] )
+			let accepted = filter.test("apple")
+			assertEquals( accepted, true )
 		},
 
 		test_string_is_empty: function() {
-			let filter = new RegexFilter( ["abc"] );
-			let accepted = filter.test("");
-			assertEquals( accepted, false );
+			let filter = new RegexFilter( ["abc"] )
+			let accepted = filter.test("")
+			assertEquals( accepted, false )
 		},
 
 		test_with_complex_pattern: function() {
-			let filter = new RegexFilter( ["kernel:.*\[UFW BLOCK\].*DST=224\.0\.0\.1"] );
-			let accepted = filter.test("May  9 17:00:43 athlonx2 kernel: [46769.866007] [UFW BLOCK] IN=eth0 OUT= MAC=01:04:bb:00:38:d5:c4:7c:1f:44:12:e0:08:08 SRC=192.168.1.1 DST=224.0.0.1 LEN=28 TOS=0x00 PREC=0x00 TTL=1 ID=21685 PROTO=2");
-			assertEquals( accepted, true );
+			let filter = new RegexFilter( ["kernel:.*\[UFW BLOCK\].*DST=224\.0\.0\.1"] )
+			let accepted = filter.test("May  9 17:00:43 athlonx2 kernel: [46769.866007] [UFW BLOCK] IN=eth0 OUT= MAC=01:04:bb:00:38:d5:c4:7c:1f:44:12:e0:08:08 SRC=192.168.1.1 DST=224.0.0.1 LEN=28 TOS=0x00 PREC=0x00 TTL=1 ID=21685 PROTO=2")
+			assertEquals( accepted, true )
 		},
 
 		test_another_complex_pattern: function() {
-			let filter = new RegexFilter(["kernel:.*USB"]);
-			let accepted = filter.test("May  9 17:08:42 athlonx2 kernel: [47248.276705] usb-storage 1-9:1.0: USB Mass Storage device detected");
-			assertEquals( accepted, true );
+			let filter = new RegexFilter(["kernel:.*USB"])
+			let accepted = filter.test("May  9 17:08:42 athlonx2 kernel: [47248.276705] usb-storage 1-9:1.0: USB Mass Storage device detected")
+			assertEquals( accepted, true )
 		}
 
-	};
+	}
 
 	let test_composeRegexFiltersLabel = {
 		test_zero_active_filters: function() {
@@ -825,21 +824,19 @@ function allTests(testDir) {
 
 		test_one_active_filter: function() {
 			let expected = " 𐏑"
-			let actual = composeRegexFiltersLabel(1);
+			let actual = composeRegexFiltersLabel(1)
 			assertEquals(actual, expected)
 		}
-
-
 	}
 
-	// Test cases (runs):
+	// Run test cases from each group:
 	runTestCases(test_readLinesFromDataStream, testDir);
 	runTestCases(test_splitString);
 	runTestCases(test_Screen);
 	runTestCases(test_RegexFilter);
 	runTestCases(test_composeRegexFiltersLabel);
 
-	global.log("TESTS OK.");
+	logInfo("TESTS ARE OK!");
 }
 
 
@@ -908,6 +905,11 @@ function getAllProperties(obj){
 		keys.push(key);
 	}
 	return keys.join("\n");
+}
+
+// Prints message into log as it is.
+function logInfo(msg) {
+	global.log(msg)
 }
 
 // Prints message into log with '>>>' prefix.
